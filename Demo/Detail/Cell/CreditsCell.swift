d0364aeb0b8b3ae8583e527d0a959117e0fc7619
Melisa Öztürk <mlsaoztrk@icloud.com>
2020-11-22 20:43:59 +0300

    ui refacto

---
+++ Demo/Detail/Cell/CreditsCell.swift
@@ -0,0 +1,17 @@
+//
+//  CreditsCell.swift
+//  Demo
+//
+//  Created by melisa öztürk on 22.11.2020.
+//
+
+import UIKit
+
+class CreditsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
